338599c29f3580b7db6f8b05badee4d470c56f4a
Jonny Gates <jonny.gates@outlook.com>
2019-04-06 11:39:29 +0100

    Add copy to projects section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const projects = document.getElementById("projects");
 
+
+
+document.getElementById("projects-btn").addEventListener("click", (event) => {
+  projects.scrollIntoView(true);
+  console.log(document.getElementById("projects-btn"));
+});
