070a68f1d423c6922a4858af0e1f0daa898bbd1e
Jonny Gates <jonny.gates@outlook.com>
2019-04-07 17:40:08 +0100

    Cleanup js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ document.getElementById("projects-btn").addEventListener("click", (event) => {
   projects.scrollIntoView(true);
   console.log(document.getElementById("projects-btn"));
 });
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
