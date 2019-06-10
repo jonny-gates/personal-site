7d672981cec623a1e5aab6ee369a884aeb34f938
Jonny Gates <jonny.gates@outlook.com>
2019-06-10 08:55:12 +0100

    Disable js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-const projects = {inView: false, element: document.getElementById("projects")};
-const home = {inView: false, element: document.getElementById("home")};
-const about = {inView: false, element: document.getElementById("about")};
-const contact = {inView: false, element: document.getElementById("contact")};
+// const projects = {inView: false, element: document.getElementById("projects")};
+// const home = {inView: false, element: document.getElementById("home")};
+// const about = {inView: false, element: document.getElementById("about")};
+// const contact = {inView: false, element: document.getElementById("contact")};
 
-let height = 0;
-let itemLocation = 0;
+// let height = 0;
+// let itemLocation = 0;
 
-let currentlyInView;
+// let currentlyInView;
 
-const minusHeight = (number) => {
-  return number - number - number;
-}
+// const minusHeight = (number) => {
+//   return number - number - number;
+// }
 
-const checkInView = (element) => {
-  currentLocation = element.getBoundingClientRect().y
-  currentHeight = element.getBoundingClientRect().height
+// const checkInView = (element) => {
+//   currentLocation = element.getBoundingClientRect().y
+//   currentHeight = element.getBoundingClientRect().height
 
-  if (currentLocation <= window.innerHeight && currentLocation >= minusHeight(currentHeight) ) {
-    return true
-  }
-  return false
-}
+//   if (currentLocation <= window.innerHeight && currentLocation >= minusHeight(currentHeight) ) {
+//     return true
+//   }
+//   return false
+// }
 
-const transitionActive = (from, to) => {
-  if (from != undefined) {
-    from.element.classList.remove('active');
-  }
-  to.element.classList.add('active');
-  currentlyInView = to
-}
+// const transitionActive = (from, to) => {
+//   if (from != undefined) {
+//     from.element.classList.remove('active');
+//   }
+//   to.element.classList.add('active');
+//   currentlyInView = to
+// }
 
-window.onscroll = function (e) {
-  projects.inView = checkInView(home.element);
-  home.inView = checkInView(about.element);
-  about.inView = checkInView(projects.element);
-  contact.inView = checkInView(contact.element);
+// window.onscroll = function (e) {
+//   projects.inView = checkInView(home.element);
+//   home.inView = checkInView(about.element);
+//   about.inView = checkInView(projects.element);
+//   contact.inView = checkInView(contact.element);
 
-  const items = [projects, home, about, contact];
+//   const items = [projects, home, about, contact];
 
-  items.forEach(function(item) {
-    if (item.inView && item != currentlyInView) {
-      console.log(item);
-      transitionActive(currentlyInView, item);
-    }
-  })
+//   items.forEach(function(item) {
+//     if (item.inView && item != currentlyInView) {
+//       console.log(item);
+//       transitionActive(currentlyInView, item);
+//     }
+//   })
 
-}
+// }
